e799df087ae6fa961a70d0a92cdd091e071f36da
Giovanni Almeida dos Santos <giovanni.DevJava@hotmail.com>
2024-07-07 23:45:26 -0300

    Created new micro-frontend to project

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,33 +1,14 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-//mostra a documentação principal
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import<LifeCycles>(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
-
-
-// registerApplication({
-//   name: "@home-hub/hello-world",
-//   app: () => System.import<LifeCycles>("@home-hub/hello-world"),
-//   //Definindo onde estara visivel
-//   // activeWhen: ["/hello-word", "/docs"]
-//   //definindo somente uma rota para ver esse servico
-//   activeWhen: (location) => location.pathname === '/hello-world'
-// });
-
 registerApplication({
   name: "@home-hub/navbar",
   app: () => System.import<LifeCycles>("@home-hub/navbar"),
-  //Definindo onde estara visivel
-  // activeWhen: ["/hello-word", "/docs"]
-  //definindo somente uma rota para ver esse servico
-  // activeWhen: (location) => location.pathname === '/'
-  // activeWhen: ["/"]
+  activeWhen: (location) => location.pathname.includes('/')
+});
+
+registerApplication({
+  name: "@home-hub/dashboard",
+  app: () => System.import<LifeCycles>("@home-hub/dashboard"),
   activeWhen: (location) => location.pathname.includes('/')
 });
 
